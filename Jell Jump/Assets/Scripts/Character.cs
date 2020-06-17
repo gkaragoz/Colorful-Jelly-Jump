848791ff56171263587c74a408fb56adfd6868bb
HasanGanover <hasan@ganover.com>
2020-06-18 01:06:19 +0300

    Added a few script

---
+++ Jell Jump/Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class Jelly : MonoBehaviour
+{
+    public Action OnInputDrag;
+
+    public Action OnInputPressed;
+
+    public Action OnInputUnPressed;
+
+    // When user press the screen
+    private void OnMouseDown()
+    {
+        OnInputPressed();
+    }
+
+    // When user release the screen
+    private void OnMouseUp()
+    {
+        OnInputUnPressed();
+    }
+
+    // While user dragging on the screen
+    private void OnMouseDrag()
+    {
+        OnInputDrag();
+    }
+}
