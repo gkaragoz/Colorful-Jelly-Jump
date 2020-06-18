1723aa9962ec019c091f5e53e17d370ea19c69bd
HasanGanover <hasan@ganover.com>
2020-06-18 08:24:55 +0300

    Added required field to Character scripts

--- Jell Jump/Assets/Scripts/MainCharacter/Character.cs
+++ Jell Jump/Assets/Scripts/MainCharacter/Character.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CharacterController), typeof(CharacterMovement))]
 public class Character : MonoBehaviour
 {
+    [SerializeField]
+    private float Health = 100;
 
+    [SerializeField]
+    private int jumpLevel = 0;
+
+    [SerializeField]
+    private int healthLevel = 0;
+
+    [SerializeField]
+    private int totalGold = 0;
+
+    // Health Level Increaser
+    public void IncreaseHealthLevel()
+    {
+        healthLevel++;
+
+        // Effect on Health
+        Health += (Health * healthLevel * 10) / Health;
+    }
+
+    // Jump Level Increaser
+    public void IncreaseJumpLevel()
+    {
+        jumpLevel++;
+    }
+
+    // Gold Increaser
+    public void IncreaseGold(int earnedGold)
+    {
+        totalGold += earnedGold;
+    }
 
     private void OnEnable()
     {
