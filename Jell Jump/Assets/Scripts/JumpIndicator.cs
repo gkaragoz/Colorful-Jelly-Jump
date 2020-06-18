5a185c226dd0033f89ef95d1e5a8e07300ff51fe
HasanGanover <hasan@ganover.com>
2020-06-18 04:19:14 +0300

    Created new empty scripts

---
+++ Jell Jump/Assets/Scripts/JumpIndicator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class JumpIndicator : MonoBehaviour
+{
+    // While button is holding
+    private void OnDrag(float dragAxis)
+    {
+        // Rotate JumpIndicator
+    }
+
+    // When button is relased
+    private void OnReleased()
+    {
+        // Hidden JumpIndicator
+
+        // Stop JumpIndicator Action
+    }
+
+    // When button is pressed
+    private void OnPressed()   
+    {
+        // Visible JumpIndicator
+
+        // Start JumpIndicator Action
+    }
+
+    private void OnEnable()
+    {
+        FloatingJoystick.OnJoystickPress += OnPressed;
+
+        FloatingJoystick.OnJoystickRelease += OnReleased;
+
+        FloatingJoystick.OnJoystickDrag += OnDrag;
+    }
+
+    private void OnDisable()
+    {
+        FloatingJoystick.OnJoystickPress -= OnPressed;
+
+        FloatingJoystick.OnJoystickRelease -= OnReleased;
+
+        FloatingJoystick.OnJoystickDrag -= OnDrag;
+    }
+}
