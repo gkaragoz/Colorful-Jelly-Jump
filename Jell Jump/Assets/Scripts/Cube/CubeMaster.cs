532f30aa717b42fa1ba71b67ac14d986cc13d06e
HasanGanover <hasan@ganover.com>
2020-06-20 01:26:38 +0300

    Created cube prefabs and re-organize project folder

---
+++ Jell Jump/Assets/Scripts/Cube/CubeMaster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CubeMaster : MonoBehaviour
+{
+    [SerializeField]
+    private CubeState _cubeState;
+
+    [SerializeField]
+    private int _damageRate;
+
+    [SerializeField]
+    private int _pointMultiplier;
+
+}
---
+++ Jell Jump/Assets/Scripts/Enums.cs
@@ -0,0 +1,8 @@
+﻿
+public enum CubeState
+{   // Defines cube's states
+    DEADLY,     // Cause character' death
+    DANGEROUSLY,    // Cause damage
+    NONEFFECTIVELY,     // No effect on Character
+    USEFULLY    // Earns golds, health, points etc.
+}
