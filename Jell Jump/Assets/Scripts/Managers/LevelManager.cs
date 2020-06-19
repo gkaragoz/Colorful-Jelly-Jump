c5a48d17d32522abfe4220acfd0be46c4005b10d
HasanGanover <hasan@ganover.com>
2020-06-20 02:50:05 +0300

    Added Level Manager

---
+++ Jell Jump/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private Character _character;
+
+    private void Awake()
+    {
+        _character = FindObjectOfType<Character>();
+    }
+
+    private void Start()
+    {
+        _character.OnCharacterDeathState += RestartLevel;
+    }
+
+    private void RestartLevel()
+    {
+        StartCoroutine("StartLevelTimer");
+    }
+
+    private IEnumerator StartLevelTimer()
+    {
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
