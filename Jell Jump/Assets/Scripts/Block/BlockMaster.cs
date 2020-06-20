d4bd75295b04304c8cd4d21b92d1169c6804e2dd
HasanGanover <hasan@ganover.com>
2020-06-20 06:40:32 +0300

    Added BlockMaster script to manage cubes inside of block

---
+++ Jell Jump/Assets/Scripts/Block/BlockMaster.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BlockMaster : MonoBehaviour
+{
+    // Makes all cubes to inactive
+    public void MakeAllChildToInactive()
+    {
+        int i = 1;
+
+        foreach (CubeMaster cube in GetComponentsInChildren<CubeMaster>())
+        {
+            // Invokes cube's deactive states
+            cube.DeactiveState(new Color(.83f, .82f, .68f), i * 0.75f);
+
+            i++;
+        }
+    }
+}
