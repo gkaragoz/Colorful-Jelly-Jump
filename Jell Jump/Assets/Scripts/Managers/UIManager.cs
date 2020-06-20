31b62e85f75b016738aba36ade2611d0dcdeb449
HasanGanover <hasan@ganover.com>
2020-06-20 03:15:29 +0300

    Commit UIManager

---
+++ Jell Jump/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _healtText;
+
+    [SerializeField]
+    private TextMeshProUGUI _pointText;
+
+    private Character _myCharacter;
+
+    private void Awake()
+    {
+        _myCharacter = FindObjectOfType<Character>();
+    }
+
+    private void Update()
+    {
+        _healtText.text = "Health: " + _myCharacter.Health();
+
+        _pointText.text = "Point: " + _myCharacter.TotalPoint();
+    }
+}
